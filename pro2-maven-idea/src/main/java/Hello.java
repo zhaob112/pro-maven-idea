68f262e2cf7127fd9cc8ac0e8c21a504a696b3d0
zhao <zhao@aiguigu.com>
2023-03-02 14:17:45 +0800

    first commit

---
+++ pro2-maven-idea/src/main/java/Hello.java
@@ -0,0 +1,13 @@
+/**
+ * @Description:
+ * @Param:
+ * @return:
+ * @Author: BIbo
+ * @Date:
+ */
+public class Hello {
+    public static void main(String[] args) {
+        String s1 = "hello";
+        System.out.println(s1);
+    }
+}
